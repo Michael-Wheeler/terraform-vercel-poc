ca938d8872acc83452b3ef737518922afa3b4ad9
Michael Wheeler <michael.wheeler@ovo.com>
2023-06-27 11:16:07 +0100

    create nonprod deployment

--- infra/main.tf
+++ infra/main.tf
@@ -7,16 +7,21 @@ terraform {
   }
 }
 
-provider "vercel" {
-  api_token = var.vercel_api_token
-  #   team = var.vercel_team_id
-}
+# provider "vercel" {
+#   api_token = var.vercel_api_token
+#   #   team = var.vercel_team_id
+# }
 
-resource "vercel_project" "infra_test_project" {
+resource "vercel_project" "infra_test" {
   name      = "infra-test-project"
   framework = "nextjs"
   git_repository = {
     type = "github"
     repo = "Michael-Wheeler/vercel-infra-test"
   }
+}
+
+resource "vercel_deployment" "infra_test" {
+  project_id = vercel_project.infra_test.id
+  ref        = "nonprod"
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,7 @@
-variable "vercel_api_token" {
-  description = "API token used during Vercel deployment"
-  type        = string
-}
+# variable "vercel_api_token" {
+#   description = "API token used during Vercel deployment"
+#   type        = string
+# }
 
 # variable "vercel_team_id" {
 #   description = "ID of the Vercel team to deploy resources to"
